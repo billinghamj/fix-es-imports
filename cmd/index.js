7c7d471c5cd865ec4fbc314e0e27d4efe8aeefb6
James Billingham <git@jamesbillingham.com>
2019-11-22 14:54:48 +0000

    implement command

---
+++ cmd/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node --experimental-modules
+
+import path from 'path';
+import resolve from 'resolve';
+import glob from 'glob';
+import fs from 'fs';
+
+const moduleDeclRegex = /^((?:import|export)\s[^';]+')(\.\.?\/[^']+)(';)$/gm;
+
+const codePaths = glob.sync('**/*.mjs');
+
+for (const codePath of codePaths) {
+	const origCode = fs.readFileSync(codePath, 'utf8');
+	const baseDir = path.dirname(codePath);
+
+	const newCode = origCode.replace(moduleDeclRegex, (_, start, origTarget, end) => {
+		const newTarget = rewriteTarget(origTarget, baseDir);
+
+		return `${start}${newTarget}${end}`;
+	});
+
+	if (newCode !== origCode)
+		fs.writeFileSync(codePath, newCode, 'utf8');
+}
+
+function rewriteTarget(original, baseDir) {
+	const absolute = resolve.sync(original, {
+		basedir: baseDir,
+		extensions: ['.js', '.node', '.mjs', '.cjs', '.json'],
+	});
+
+	const relative = path.relative(baseDir, absolute);
+
+	return `./${relative}`.replace(/^\.\/\.\./, '..');
+}
